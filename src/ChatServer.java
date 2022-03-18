2f56d9a44032c4883d057f652aa24b2f0d97df64
Bislan <mamaliev.bislan@mail.ru>
2022-03-18 12:09:49 +0300

    Chat done!

---
+++ src/ChatServer.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ChatServer {
+    ArrayList<Client> clients = new ArrayList<>();
+    ServerSocket serverSocket;
+    ChatServer() throws IOException {
+        serverSocket = new ServerSocket(1234);
+    }
+
+    void sandAll(String massage) {
+        for (Client client : clients) {
+            client.receive(massage);
+        }
+    }
+
+    public void run() {
+        while (true) {
+            System.out.println("Waiting...");
+            try {
+                Socket socket = serverSocket.accept();
+                System.out.println("Client connected!");
+                clients.add(new Client(socket, this));
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        new ChatServer().run();
+    }
+}
